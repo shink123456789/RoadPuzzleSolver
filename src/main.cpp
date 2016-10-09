404f5ff62b4764649dc3085b0ad7a699fb119cbd
S.K <sk@anpan.test_domain>
2016-10-09 18:13:06 +0900

    delete gtest code

--- src/main.cpp
+++ src/main.cpp
@@ -5,25 +5,16 @@
  *      Author: sk
  */
 
-#include <gtest/gtest.h>
 #include <cstdio>
-
-//#define GTEST_DEBUG
+#include <iostream>
 
 #include "RoadPuzzleSolver.h"
 
-
-#ifdef GTEST_DEBUG
-int main(int argc, char** argv){
-	::testing::InitGoogleTest(&argc,argv);
-	return RUN_ALL_TESTS();
-#else
 int main(){
 	RoadPuzzleSolver solver;
 	std::cout << "RoadPuzzleSolver created." << std::endl;
 	solver.solve();
 	return 0;
-#endif
 }
 
 
